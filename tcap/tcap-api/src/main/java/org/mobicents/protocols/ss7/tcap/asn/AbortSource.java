f63f323d185de5b2ea94bca05eaced6cee9a44e6
baranowb <baranowb@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-04-09 16:11:06 +0000

    fixes against : coded V vs asn vs specs ... everything is different..
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/protocols/ss7@11121 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- tcap/tcap-api/src/main/java/org/mobicents/protocols/ss7/tcap/asn/AbortSource.java
+++ tcap/tcap-api/src/main/java/org/mobicents/protocols/ss7/tcap/asn/AbortSource.java
@@ -12,9 +12,9 @@ import org.mobicents.protocols.asn.Tag;
 public interface AbortSource extends Encodable {
 
 	//Q773 shows this is PRIMITIVE ..... but its coded in ASN the same way as Result.....
-	//making it COnstructed, as other.....
+	//making it Primitive...., where is logic!
 	public static final int _TAG_CLASS = Tag.CLASS_CONTEXT_SPECIFIC;
-	public static final boolean _TAG_PC_PRIMITIVE = false; //constructed.... // specs show true
+	public static final boolean _TAG_PC_PRIMITIVE = true; //constructed.... // specs show true
 	public static final int _TAG = 0x00;
 	
 	public void setAbortSourceType(AbortSourceType t);
--- tcap/tcap-impl/src/main/java/org/mobicents/protocols/ss7/tcap/asn/AbortSourceImpl.java
+++ tcap/tcap-impl/src/main/java/org/mobicents/protocols/ss7/tcap/asn/AbortSourceImpl.java
@@ -37,19 +37,31 @@ public class AbortSourceImpl implements AbortSource {
 	 * @see org.mobicents.protocols.ss7.tcap.asn.Encodable#decode(org.mobicents.protocols.asn.AsnInputStream)
 	 */
 	public void decode(AsnInputStream ais) throws ParseException {
-		int len;
+//		int len;
+//		try {
+//			len = ais.readLength();
+//
+//			if (len > ais.available()) {
+//				throw new ParseException("Not enough data: " + ais.available());
+//			}
+//
+//			int tag = ais.readTag();
+//			if (tag != Tag.INTEGER) {
+//				throw new ParseException("Expected INTEGER tag, found: " + tag);
+//
+//			}
+//			// y, its a bit of enum, should be ok to cast :)
+//			long t = ais.readInteger();
+//			this.type = AbortSourceType.Provider.getFromInt(t);
+//		} catch (IOException e) {
+//			throw new ParseException(e);
+//		} catch (AsnException e) {
+//			throw new ParseException(e);
+//		}
+
+		
 		try {
-			len = ais.readLength();
-
-			if (len > ais.available()) {
-				throw new ParseException("Not enough data: " + ais.available());
-			}
-
-			int tag = ais.readTag();
-			if (tag != Tag.INTEGER) {
-				throw new ParseException("Expected INTEGER tag, found: " + tag);
-
-			}
+			
 			// y, its a bit of enum, should be ok to cast :)
 			long t = ais.readInteger();
 			this.type = AbortSourceType.Provider.getFromInt(t);
@@ -58,7 +70,7 @@ public class AbortSourceImpl implements AbortSource {
 		} catch (AsnException e) {
 			throw new ParseException(e);
 		}
-
+		
 	}
 
 	/* (non-Javadoc)
@@ -68,16 +80,22 @@ public class AbortSourceImpl implements AbortSource {
 		if (type == null) {
 			throw new ParseException("No type set!");
 		}
+//		try {
+//			AsnOutputStream localAos = new AsnOutputStream();
+//			localAos.writeInteger(this.type.getType());
+//			byte[] b = localAos.toByteArray();
+//			aos.writeTag(_TAG_CLASS, _TAG_PC_PRIMITIVE, _TAG);
+//			aos.writeLength(b.length);
+//			aos.write(b);
+//		} catch (IOException e) {
+//			throw new ParseException(e);
+//		}
 		try {
-			AsnOutputStream localAos = new AsnOutputStream();
-			localAos.writeInteger(this.type.getType());
-			byte[] b = localAos.toByteArray();
-			aos.writeTag(_TAG_CLASS, _TAG_PC_PRIMITIVE, _TAG);
-			aos.writeLength(b.length);
-			aos.write(b);
-		} catch (IOException e) {
-			throw new ParseException(e);
-		}
+		
+		aos.writeInteger(_TAG_CLASS,_TAG,type.getType());
+	} catch (IOException e) {
+		throw new ParseException(e);
+	}
 
 	}
 
