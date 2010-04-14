ebbb7ad3d702bc2eaabe09e600dff9bdde318cd7
baranowb <baranowb@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-04-14 10:51:59 +0000

    git-svn-id: https://mobicents.googlecode.com/svn/trunk/protocols/ss7@11157 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- tcap/tcap-impl/src/test/java/org/mobicents/protocols/ss7/tcap/asn/TcBeginTest.java
+++ tcap/tcap-impl/src/test/java/org/mobicents/protocols/ss7/tcap/asn/TcBeginTest.java
@@ -14,6 +14,8 @@ import org.mobicents.protocols.ss7.tcap.asn.comp.ComponentType;
 import org.mobicents.protocols.ss7.tcap.asn.comp.Invoke;
 import org.mobicents.protocols.ss7.tcap.asn.comp.OperationCode;
 import org.mobicents.protocols.ss7.tcap.asn.comp.OperationCodeType;
+import org.mobicents.protocols.ss7.tcap.asn.comp.ReturnResult;
+import org.mobicents.protocols.ss7.tcap.asn.comp.ReturnResultLast;
 import org.mobicents.protocols.ss7.tcap.asn.comp.TCBeginMessage;
 import org.mobicents.protocols.ss7.tcap.asn.comp.TCContinueMessage;
 
@@ -122,5 +124,526 @@ public class TcBeginTest extends TestCase {
 	}
 
 	
+	@org.junit.Test
+	public void testTCBeginMessage_No_Dialog() throws IOException, ParseException {
+
+		
+		
+		
+		
+		//no idea how to check rest...?
+		
+		//created by hand
+		byte[] b = new byte[]{
+			//TCBegin
+			0x62,
+				68,
+				//org txid
+				//OrigTran ID (full)............ 145031169 
+				0x48,
+					0x04,
+					0x08,
+					(byte) 0xA5,
+					0,
+					0x01,
+				//dtx
+				
+				//dialog portion
+				//empty
+				//comp portion
+				0x6C,
+				60,
+				//invoke
+				(byte) 0xA1,
+					6,
+					//invoke ID
+					0x02,
+					0x01,
+					0x01,
+					//op code
+					0x02,
+					0x01,
+					0x37,
+				//return result
+				(byte) 0xA7,
+					50,
+					//inoke id
+					0x02,
+					0x01,
+					0x02,
+					//sequence start
+					0x30,
+					7,
+					//	local operation
+						0x02,
+						0x01,
+						0x01,
+					// 	global operation
+						0x06,
+						0x02,
+						0x00,
+						(byte) 0xFF,
+				//	parameter
+					0x30,
+					36,
+					(byte) 0xA0,//some tag.1
+					17,
+					(byte) 0x80,//some tag.1.1
+					2,
+					0x11,
+					0x11,
+					(byte) 0xA1,//some tag.1.2
+					04,
+					(byte)0x82, //some tag.1.3 ?
+					2,
+					0x00,
+					0x00,
+					(byte) 0x82,
+					//some tag.1.4
+					1,
+					12,
+					(byte)0x83, //some tag.1.5
+					2,
+					0x33,
+					0x33,
+					(byte) 0xA1,//some trash here
+					//tension indicator 2........ ???
+					//use value.................. ???
+					//some tag.2
+					3,
+					(byte) 0x80,//some tag.2.1
+					1,
+					-1,
+					(byte)0xA2, //some tag.3
+					3,
+					(byte) 0x80,//some tag.3.1
+					1,
+					-1,
+					(byte) 0xA3,//some tag.4
+					5,
+					(byte) 0x82,//some tag.4.1
+					3,
+					(byte) 0xAB,// - 85 serviceKey................... 123456 // dont care about this content, lets just make len correct
+					(byte) 0xCD,
+					(byte) 0xEF
+				};
+		
+		AsnInputStream ais = new AsnInputStream(new ByteArrayInputStream(b));
+		int tag = ais.readTag();
+		assertEquals("Expected TCBegin",TCBeginMessage._TAG,tag);
+		TCBeginMessage tcm = TcapFactory.createTCBeginMessage(ais);
+		
+		assertNull("Dialog portion should not be present",tcm.getDialogPortion());
+
+		assertEquals("Originating transaction id does not match",new Long(145031169L), tcm.getOriginatingTransactionId());
+		//comp portion
+		assertNotNull("Component portion should be present",tcm.getComponent());
+		assertEquals("Component count is wrong",2,tcm.getComponent().length);
+		Component c = tcm.getComponent()[0];
+		assertEquals("Wrong component type",ComponentType.Invoke, c.getType());
+		Invoke i = (Invoke) c;
+		assertEquals("Wrong invoke ID",new Long(1), i.getInvokeId());
+		assertNull("Linked ID is not null", i.getLinkedId());
+		
+		c = tcm.getComponent()[1];
+		assertEquals("Wrong component type",ComponentType.ReturnResult, c.getType());
+		ReturnResult rr = (ReturnResult) c;
+		assertEquals("Wrong invoke ID",new Long(2), rr.getInvokeId());
+		assertNotNull("Operation code should not be null", rr.getOperationCode());
+		assertEquals("Operation Code count is wrong ",2, rr.getOperationCode().length);
+		
+		OperationCode[] ocs = rr.getOperationCode();
+		
+		assertEquals("Wrong Operation Code type",OperationCodeType.Local, ocs[0].getOperationType());
+		assertEquals("Wrong Operation Code",new Long(1), ocs[0].getCode());
+		assertEquals("Wrong Operation Code type",OperationCodeType.Global, ocs[1].getOperationType());
+		assertEquals("Wrong Operation Code",new Long(0x00FF), ocs[1].getCode());
+		
+		assertNotNull("Parameter should not be null",rr.getParameter());
+		
+		AsnOutputStream aos = new AsnOutputStream();
+		tcm.encode(aos);
+		byte[] encoded = aos.toByteArray();
+		
+		compareArrays(b,encoded);
+
+	}
+
+	@org.junit.Test
+	public void testTCBeginMessage_No_Component() throws IOException, ParseException {
+
+		
+		
+		//created by hand
+		byte[] b = new byte[]{
+				//TCBegin
+				0x62,
+					50,
+					//org txid
+					//OrigTran ID (full)............ 145031169 
+					0x48,
+						0x04,
+						0x08,
+						(byte) 0xA5,
+						0,
+						0x01,
+					
+					//dialog portion
+					0x6B,
+						42,
+						//extrnal tag
+						0x28,
+						40,
+						//oid
+							0x06,
+							7,
+							0,
+							17,
+							(byte) 134,
+							5,
+							1,
+							1,
+							1,
+							(byte)160, //asn
+							
+								29,
+								0x61,	//dialog response
+									27,
+									//protocol version
+									(byte)0x80, //protocol version
+									
+										2,
+										7,
+									(byte) 0x80,
+									(byte) 161,//acn 
+										9,
+										//oid
+										6,
+										7,
+										4,
+										0,
+										1,
+										1,
+										1,
+										3,
+										0,
+									//result
+								(byte)0xA2,
+										0x03,
+										0x2, 
+											0x1, 
+											(byte) 0x0,
+									//result source diagnostic
+									(byte)0xA3,
+										5,
+								  (byte)0x0A2, //provider
+											3,
+											0x02,//int 2
+											0x01,
+									  (byte)0x2
+									//no user info?
+		};
+		
+		AsnInputStream ais = new AsnInputStream(new ByteArrayInputStream(b));
+		int tag = ais.readTag();
+		assertEquals("Expected TCBegin",TCBeginMessage._TAG,tag);
+		TCBeginMessage tcm = TcapFactory.createTCBeginMessage(ais);
+		assertNull("Component portion should not be present",tcm.getComponent());
+		assertNotNull("Dialog portion should not be null",tcm.getDialogPortion());
+		assertEquals("Originating transaction id does not match",new Long(145031169L), tcm.getOriginatingTransactionId());
+		
+		assertFalse("Dialog should not be Uni", tcm.getDialogPortion().isUnidirectional());
+		DialogAPDU _dapd = tcm.getDialogPortion().getDialogAPDU();
+		assertEquals("Wrong dialog APDU type!",DialogAPDUType.Response, _dapd.getType());
+		
+		DialogResponseAPDU dapd = (DialogResponseAPDU) _dapd;
+		
+		//check nulls first
+		assertNull("UserInformation should not be present",dapd.getUserInformation());
+		
+		//not nulls
+		assertNotNull("Result should not be null", dapd.getResult());
+		Result r = dapd.getResult();
+		assertEquals("Wrong result",ResultType.Accepted ,r.getResultType());
+		
+		
+		assertNotNull("Result Source Diagnostic should not be null",dapd.getResultSourceDiagnostic());
+		
+		ResultSourceDiagnostic rsd = dapd.getResultSourceDiagnostic();
+		assertNull("User diagnostic should not be present",rsd.getDialogServiceUserType());
+		assertEquals("Wrong provider diagnostic type",DialogServiceProviderType.NoCommonDialogPortion,rsd.getDialogServiceProviderType());
+		
+		AsnOutputStream aos = new AsnOutputStream();
+		tcm.encode(aos);
+		byte[] encoded = aos.toByteArray();
+		
+		compareArrays(b,encoded);
+
+	}
+	@org.junit.Test
+	public void testTCBeginMessage_No_Nothing() throws IOException, ParseException {
+
+		
+		
+		
+		
+		//no idea how to check rest...?
+		
+		//created by hand
+		byte[] b = new byte[]{
+				//TCBegin
+				0x62,
+					6,
+					//org txid
+					//OrigTran ID (full)............ 145031169 
+					0x48,
+						0x04,
+						0x08,
+						(byte) 0xA5,
+						0,
+						0x01,
+					//didTx
+				
+		
+		};
+		
+		AsnInputStream ais = new AsnInputStream(new ByteArrayInputStream(b));
+		int tag = ais.readTag();
+		assertEquals("Expected TCBegin",TCBeginMessage._TAG,tag);
+		TCBeginMessage tcm = TcapFactory.createTCBeginMessage(ais);
+		
+		assertNull("Dialog portion should be null",tcm.getDialogPortion());
+		assertNull("Component portion should not be present",tcm.getComponent());
+		assertEquals("Originating transaction id does not match",new Long(145031169L), tcm.getOriginatingTransactionId());
+	
+		AsnOutputStream aos = new AsnOutputStream();
+		tcm.encode(aos);
+		byte[] encoded = aos.toByteArray();
+		
+		compareArrays(b,encoded);
+
+	}
+	
+	@org.junit.Test
+	public void testTCBeginMessage_All() throws IOException, ParseException {
+
+		
+		
+		
+		
+		//no idea how to check rest...?
+		
+		//created by hand
+		byte[] b = new byte[]{
+				//TCBegin
+				0x62,
+					68+44,
+					//org txid
+					//OrigTran ID (full)............ 145031169 
+					0x48,
+						0x04,
+						0x08,
+						(byte) 0xA5,
+						0,
+						0x01,
+					
+					//dialog portion
+						0x6B,
+						42,
+						//extrnal tag
+						0x28,
+						40,
+						//oid
+							0x06,
+							7,
+							0,
+							17,
+							(byte) 134,
+							5,
+							1,
+							1,
+							1,
+							(byte)160, //asn
+							
+								29,
+								0x61,	//dialog response
+									27,
+									//protocol version
+									(byte)0x80, //protocol version
+									
+										2,
+										7,
+									(byte) 0x80,
+									(byte) 161,//acn 
+										9,
+										//oid
+										6,
+										7,
+										4,
+										0,
+										1,
+										1,
+										1,
+										3,
+										0,
+									//result
+								(byte)0xA2,
+										0x03,
+										0x2, 
+											0x1, 
+											(byte) 0x01,
+									//result source diagnostic
+									(byte)0xA3,
+										5,
+								  (byte)0x0A2, //provider
+											3,
+											0x02,//int 2
+											0x01,
+									  (byte)0x00,
+									//no user info?
+					//comp portion
+					0x6C,
+					60,
+					//invoke
+					(byte) 0xA1,
+						6,
+						//invoke ID
+						0x02,
+						0x01,
+						0x01,
+						//op code
+						0x02,
+						0x01,
+						0x37,
+					//return result last
+					(byte) 0xA2,
+						50,
+						//inoke id
+						0x02,
+						0x01,
+						0x02,
+						//sequence start
+						0x30,
+						7,
+						//	local operation
+							0x02,
+							0x01,
+							0x01,
+						// 	global operation
+							0x06,
+							0x02,
+							0x00,
+							(byte) 0xFF,
+					//	parameter
+						0x30,
+						36,
+						(byte) 0xA0,//some tag.1
+						17,
+						(byte) 0x80,//some tag.1.1
+						2,
+						0x11,
+						0x11,
+						(byte) 0xA1,//some tag.1.2
+						04,
+						(byte)0x82, //some tag.1.3 ?
+						2,
+						0x00,
+						0x00,
+						(byte) 0x82,
+						//some tag.1.4
+						1,
+						12,
+						(byte)0x83, //some tag.1.5
+						2,
+						0x33,
+						0x33,
+						(byte) 0xA1,//some trash here
+						//tension indicator 2........ ???
+						//use value.................. ???
+						//some tag.2
+						3,
+						(byte) 0x80,//some tag.2.1
+						1,
+						-1,
+						(byte)0xA2, //some tag.3
+						3,
+						(byte) 0x80,//some tag.3.1
+						1,
+						-1,
+						(byte) 0xA3,//some tag.4
+						5,
+						(byte) 0x82,//some tag.4.1
+						3,
+						(byte) 0xAB,// - 85 serviceKey................... 123456 // dont care about this content, lets just make len correct
+						(byte) 0xCD,
+						(byte) 0xEF
+					};
+		
+		AsnInputStream ais = new AsnInputStream(new ByteArrayInputStream(b));
+		int tag = ais.readTag();
+		assertEquals("Expected TCBegin",TCBeginMessage._TAG,tag);
+		TCBeginMessage tcm = TcapFactory.createTCBeginMessage(ais);
+		
+		//universal
+		assertEquals("Originating transaction id does not match",new Long(145031169L), tcm.getOriginatingTransactionId());
+		
+		//dialog portion
+		assertNotNull("Dialog portion should not be null",tcm.getDialogPortion());
+
+		
+		assertFalse("Dialog should not be Uni", tcm.getDialogPortion().isUnidirectional());
+		DialogAPDU _dapd = tcm.getDialogPortion().getDialogAPDU();
+		assertEquals("Wrong dialog APDU type!",DialogAPDUType.Response, _dapd.getType());
+		
+		DialogResponseAPDU dapd = (DialogResponseAPDU) _dapd;
+		
+		//check nulls first
+		assertNull("UserInformation should not be present",dapd.getUserInformation());
+		
+		//not nulls
+		assertNotNull("Result should not be null", dapd.getResult());
+		Result r = dapd.getResult();
+		assertEquals("Wrong result",ResultType.RejectedPermanent ,r.getResultType());
+		
+		
+		assertNotNull("Result Source Diagnostic should not be null",dapd.getResultSourceDiagnostic());
+		
+		ResultSourceDiagnostic rsd = dapd.getResultSourceDiagnostic();
+		assertNull("User diagnostic should not be present",rsd.getDialogServiceUserType());
+		assertEquals("Wrong provider diagnostic type",DialogServiceProviderType.Null,rsd.getDialogServiceProviderType());
+		
+		//comp portion
+		assertNotNull("Component portion should be present",tcm.getComponent());
+		assertEquals("Component count is wrong",2,tcm.getComponent().length);
+		Component c = tcm.getComponent()[0];
+		assertEquals("Wrong component type",ComponentType.Invoke, c.getType());
+		Invoke i = (Invoke) c;
+		assertEquals("Wrong invoke ID",new Long(1), i.getInvokeId());
+		assertNull("Linked ID is not null", i.getLinkedId());
+		
+		c = tcm.getComponent()[1];
+		assertEquals("Wrong component type",ComponentType.ReturnResultLast, c.getType());
+		ReturnResultLast rrl = (ReturnResultLast) c;
+		assertEquals("Wrong invoke ID",new Long(2), rrl.getInvokeId());
+		assertNotNull("Operation code should not be null", rrl.getOperationCode());
+		assertEquals("Operation Code count is wrong ",2, rrl.getOperationCode().length);
+		
+		OperationCode[] ocs = rrl.getOperationCode();
+		
+		assertEquals("Wrong Operation Code type",OperationCodeType.Local, ocs[0].getOperationType());
+		assertEquals("Wrong Operation Code",new Long(1), ocs[0].getCode());
+		assertEquals("Wrong Operation Code type",OperationCodeType.Global, ocs[1].getOperationType());
+		assertEquals("Wrong Operation Code",new Long(0x00FF), ocs[1].getCode());
+		
+		assertNotNull("Parameter should not be null",rrl.getParameter());
+		
+		
+		AsnOutputStream aos = new AsnOutputStream();
+		tcm.encode(aos);
+		byte[] encoded = aos.toByteArray();
+		
+		compareArrays(b,encoded);
+
+	}
 	
 }
