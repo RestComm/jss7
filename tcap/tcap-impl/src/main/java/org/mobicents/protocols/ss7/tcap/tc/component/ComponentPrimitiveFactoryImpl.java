26907a3b2df39b5b353e00a9cc74151866deee11
baranowb <baranowb@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-04-27 14:34:22 +0000

    git-svn-id: https://mobicents.googlecode.com/svn/trunk/protocols/ss7@11284 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- tcap/tcap-impl/src/main/java/org/mobicents/protocols/ss7/tcap/tc/component/ComponentPrimitiveFactoryImpl.java
+++ tcap/tcap-impl/src/main/java/org/mobicents/protocols/ss7/tcap/tc/component/ComponentPrimitiveFactoryImpl.java
@@ -3,17 +3,12 @@
  */
 package org.mobicents.protocols.ss7.tcap.tc.component;
 
+import org.mobicents.protocols.ss7.tcap.TCAProviderImpl;
 import org.mobicents.protocols.ss7.tcap.api.ComponentPrimitiveFactory;
-import org.mobicents.protocols.ss7.tcap.api.tc.component.TCInvokeIndication;
-import org.mobicents.protocols.ss7.tcap.api.tc.component.TCInvokeRequest;
-import org.mobicents.protocols.ss7.tcap.api.tc.component.TCRejectIndication;
-import org.mobicents.protocols.ss7.tcap.api.tc.component.TCRejectRequest;
-import org.mobicents.protocols.ss7.tcap.api.tc.component.TCResultIndication;
-import org.mobicents.protocols.ss7.tcap.api.tc.component.TCResultRequest;
-import org.mobicents.protocols.ss7.tcap.asn.comp.ComponentType;
+import org.mobicents.protocols.ss7.tcap.asn.InvokeImpl;
+import org.mobicents.protocols.ss7.tcap.asn.TcapFactory;
 import org.mobicents.protocols.ss7.tcap.asn.comp.Invoke;
 import org.mobicents.protocols.ss7.tcap.asn.comp.Reject;
-import org.mobicents.protocols.ss7.tcap.asn.comp.Return;
 import org.mobicents.protocols.ss7.tcap.asn.comp.ReturnResult;
 import org.mobicents.protocols.ss7.tcap.asn.comp.ReturnResultLast;
 
@@ -23,20 +18,10 @@ import org.mobicents.protocols.ss7.tcap.asn.comp.ReturnResultLast;
  */
 public class ComponentPrimitiveFactoryImpl implements ComponentPrimitiveFactory {
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seeorg.mobicents.protocols.ss7.tcap.api.ComponentPrimitiveFactory#
-	 * createTCInvokeIndication
-	 * (org.mobicents.protocols.ss7.tcap.asn.comp.Invoke)
-	 */
-	public TCInvokeIndication createTCInvokeIndication(Invoke i) {
-		if (i == null) {
-			throw new NullPointerException("Argument must not be null");
-		}
-		TCInvokeIndicationImpl t = new TCInvokeIndicationImpl(i);
-
-		return t;
+	private TCAProviderImpl provider;
+	
+	public ComponentPrimitiveFactoryImpl(TCAProviderImpl tcaProviderImpl) {
+		this.provider = tcaProviderImpl;
 	}
 
 	/*
@@ -45,25 +30,10 @@ public class ComponentPrimitiveFactoryImpl implements ComponentPrimitiveFactory 
 	 * @seeorg.mobicents.protocols.ss7.tcap.api.ComponentPrimitiveFactory#
 	 * createTCInvokeRequest()
 	 */
-	public TCInvokeRequest createTCInvokeRequest() {
+	public Invoke createTCInvokeRequest() {
 
-		TCInvokeRequestImpl t = new TCInvokeRequestImpl();
-		return t;
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seeorg.mobicents.protocols.ss7.tcap.api.ComponentPrimitiveFactory#
-	 * createTCRejectIndication
-	 * (org.mobicents.protocols.ss7.tcap.asn.comp.Reject)
-	 */
-	public TCRejectIndication createTCRejectIndication(Reject rej) {
-		if (rej == null) {
-			throw new NullPointerException("Argument must not be null");
-		}
-		TCRejectIndicationImpl t = new TCRejectIndicationImpl(rej);
-
+		InvokeImpl t = (InvokeImpl) TcapFactory.createComponentInvoke();
+		t.setProvider(provider);
 		return t;
 	}
 
@@ -73,12 +43,12 @@ public class ComponentPrimitiveFactoryImpl implements ComponentPrimitiveFactory 
 	 * @seeorg.mobicents.protocols.ss7.tcap.api.ComponentPrimitiveFactory#
 	 * createTCRejectRequest()
 	 */
-	public TCRejectRequest createTCRejectRequest() {
+	public Reject createTCRejectRequest() {
 
-		TCRejectRequestImpl t = new TCRejectRequestImpl();
-
-		return t;
+		return TcapFactory.createComponentReject();
 	}
+	
+
 
 	/*
 	 * (non-Javadoc)
@@ -86,31 +56,14 @@ public class ComponentPrimitiveFactoryImpl implements ComponentPrimitiveFactory 
 	 * @seeorg.mobicents.protocols.ss7.tcap.api.ComponentPrimitiveFactory#
 	 * createTCResultRequest(boolean)
 	 */
-	public TCResultRequest createTCResultRequest(boolean last) {
-
-		TCResultRequestImpl t = new TCResultRequestImpl(last);
-
-		return t;
+	public ReturnResultLast createTCResultLastRequest() {
+		
+		return TcapFactory.createComponentReturnResultLast();
+		
 	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @seeorg.mobicents.protocols.ss7.tcap.api.ComponentPrimitiveFactory#
-	 * createTCResultRequest(org.mobicents.protocols.ss7.tcap.asn.comp.Return)
-	 */
-	public TCResultIndication createTCResultRequest(Return ret) {
-		if (ret == null) {
-			throw new NullPointerException("Argument must not be null");
-		}
-
-		if (ret.getType() == ComponentType.ReturnResult) {
-			TCResultIndicationImpl t = new TCResultIndicationImpl((ReturnResult) ret);
-			return t;
-		} else {
-			TCResultIndicationImpl t = new TCResultIndicationImpl((ReturnResultLast) ret);
-			return t;
-		}
+	public ReturnResult createTCResultRequest() {
+		
+		return TcapFactory.createComponentReturnResult();
 	}
-
+	
 }
