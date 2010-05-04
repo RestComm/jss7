7479d4856cbeff0a71aa4dbdabdfd4ea617840b0
baranowb <baranowb@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-05-04 08:39:39 +0000

    git-svn-id: https://mobicents.googlecode.com/svn/trunk/protocols/ss7@11412 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ mtp/src/main/java/org/mobicents/protocols/ss7/stream/StreamForwarder.java
@@ -0,0 +1,29 @@
+package org.mobicents.protocols.ss7.stream;
+
+import java.net.UnknownHostException;
+
+import org.mobicents.protocols.ss7.mtp.Mtp3;
+
+
+
+
+
+/**
+ * Interface for forwarding classes(TCP/SCTP)
+ * @author baranowb
+ *
+ */
+public interface StreamForwarder {
+
+	public void setPort(int port);
+	public int getPort();
+	public void setAddress(String address) throws UnknownHostException;
+	public String getAddress();
+	
+	public void setLayer3(Mtp3 layer3);
+	public void setServiceIndicator(int i);
+	public void setSubServiceIndicator(int i);
+	public void start() throws Exception;
+	public void stop();
+
+}
