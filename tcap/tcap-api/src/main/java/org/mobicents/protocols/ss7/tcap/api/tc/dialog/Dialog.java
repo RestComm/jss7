8ccb311fc0eaf367115a3bf5115a94eeec52392e
baranowb <baranowb@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-06-10 08:45:27 +0000

    git-svn-id: https://mobicents.googlecode.com/svn/trunk/protocols/ss7@12173 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- tcap/tcap-api/src/main/java/org/mobicents/protocols/ss7/tcap/api/tc/dialog/Dialog.java
+++ tcap/tcap-api/src/main/java/org/mobicents/protocols/ss7/tcap/api/tc/dialog/Dialog.java
@@ -13,9 +13,10 @@ import org.mobicents.protocols.ss7.tcap.asn.UserInformation;
 import org.mobicents.protocols.ss7.tcap.asn.comp.Component;
 
 /**
- * Inteface for class representing Dialog/Transaction. 
+ * Inteface for class representing Dialog/Transaction.
+ * 
  * @author baranowb
- *
+ * 
  */
 public interface Dialog {
 
@@ -41,40 +42,46 @@ public interface Dialog {
 	 */
 	public SccpAddress getRemoteAddress();
 
-
 	/**
 	 * Last sent/received ACN
+	 * 
 	 * @return the acn
 	 */
-	public ApplicationContextName getApplicationContextName() ;
+	public ApplicationContextName getApplicationContextName();
+
 	/**
 	 * Last sent/received UI
+	 * 
 	 * @return the ui
 	 */
-	public UserInformation getUserInformation() ;
-	
+	public UserInformation getUserInformation();
+
 	/**
 	 * returns new, unique for this dialog, invocation id to be used in
-	 * TC_INVOKE. If there is no free invoke id, it returns null
+	 * TC_INVOKE. If there is no free invoke id, it returns null. Invoke ID is
+	 * freed once operation using it is canceled, timeouts or simply returns
+	 * final value.
 	 * 
 	 * @return
 	 */
 	public Long getNewInvokeId() throws TCAPException;
 
 	/**
-	 * Cancels INVOKE pending to be sent. It is equivalent of TC-U-CANCEL.
-	 * @return
-	 * <ul>
-	 * <li><b>true</b> - </li>
-	 * <li><b>false</b> - </li>
-	 * </ul>
-	 * @throws TCAPException - thrown if passed invoke id is wrong
+	 * Cancels INVOKE pending to be sent. It is equivalent to TC-U-CANCEL.
+	 * 
+	 * @return <ul>
+	 *         <li><b>true</b> - if operation has been success and invoke id has been return to pool of available ids.</li>
+	 *         <li><b>false</b> -</li>
+	 *         </ul>
+	 * @throws TCAPException
+	 *             - thrown if passed invoke id is wrong
 	 */
 	public boolean cancelInvocation(Long invokeId) throws TCAPException;
+
 	/**
 	 * 
 	 * @return <ul>
-	 *         <li><b>true </b></li> - if dialog is established(at least on
+	 *         <li><b>true </b></li> - if dialog is established(at least one
 	 *         TC_CONTINUE has been sent/received.)
 	 *         <li><b>false</b></li> - no TC_CONTINUE sent/received
 	 *         </ul>
@@ -95,37 +102,60 @@ public interface Dialog {
 	// Sender methods //
 	// //////////////////
 	/**
-	 * Schedules component for sending. All components on list are queued. If
-	 * used passes cancel component, canceled component is removed and
-	 * indication passed to TC User
+	 * Schedules component for sending. All components on list are queued. Components are sent once message primitive is issued.
 	 * 
 	 * @param componentRequest
-	 * @throws TCAPSendException 
+	 * @throws TCAPSendException
 	 */
 	public void sendComponent(Component componentRequest) throws TCAPSendException;
-	
+	/**
+	 * Send initial primitive for Structured dialog. 
+	 * @param event
+	 * @throws TCAPSendException - thrown if dialog is in bad state, ie. Being has already been sent or dialog has been removed.
+	 */
 	public void send(TCBeginRequest event) throws TCAPSendException;
-
+	/**
+	 * Sends intermediate primitive for Structured dialog.
+	 * @param event
+	 * @throws TCAPSendException - thrown if dialog is in bad state, ie. Begin has not been sent or dialog has been removed.
+	 */
 	public void send(TCContinueRequest event) throws TCAPSendException;
-
+	/**
+	 * Sends dialog end request.
+	 * @param event
+	 * @throws TCAPSendException - thrown if dialog is in bad state, ie. Begin has not been sent or dialog has been removed.
+	 */
 	public void send(TCEndRequest event) throws TCAPSendException;
-
+	/**
+	 * Sends Abort primitive with indication to user as source of termination.
+	 * @param event
+	 * @throws TCAPSendException
+	 */
 	public void send(TCUserAbortRequest event) throws TCAPSendException;
-	
+	/**
+	 * Sends unstructured dialog primitive. After this method returns dialog is expunged from stack as its life cycle reaches end.
+	 * @param event
+	 * @throws TCAPSendException
+	 */
 	public void send(TCUniRequest event) throws TCAPSendException;
+
 	/**
 	 * Programmer hook to release.
 	 */
 	public void release();
-
-	
-	public void  resetTimer(Long invokeId) throws TCAPException;
 	
 	/**
+	 * Resets timeout timer for particular operation.
+	 * @param invokeId
+	 * @throws TCAPException
+	 */
+	public void resetTimer(Long invokeId) throws TCAPException;
+
+	/**
 	 * Returns the state of this Dialog
+	 * 
 	 * @return
 	 */
 	public TRPseudoState getState();
 
-	
 }
