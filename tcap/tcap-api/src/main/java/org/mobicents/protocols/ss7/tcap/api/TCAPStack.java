46981b54bd0d6f3b643677e31e87914c4220cb7e
baranowb <baranowb@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-07-14 13:15:23 +0000

    git-svn-id: https://mobicents.googlecode.com/svn/trunk/protocols/ss7@12980 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- tcap/tcap-api/src/main/java/org/mobicents/protocols/ss7/tcap/api/TCAPStack.java
+++ tcap/tcap-api/src/main/java/org/mobicents/protocols/ss7/tcap/api/TCAPStack.java
@@ -23,8 +23,16 @@ public interface TCAPStack {
 	 * Stops this stack and transport layer(SCCP)
 	 */
 	public void stop();
-	
+	/**
+	 * Start stack and transport layer(SCCP)
+	 * @throws IllegalStateException - if stack is already running or not configured
+	 * @throws StartFailedException
+	 */
 	public void start() throws IllegalStateException, StartFailedException;
-	
+	/**
+	 * Configure stack and transport layer.
+	 * @param properties
+	 * @throws ConfigurationException
+	 */
 	public void configure(Properties properties) throws ConfigurationException;
 }
