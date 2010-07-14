65bce02cbc444c681f11986285da4000378f6782
oleg.kulikoff <oleg.kulikoff@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-07-14 11:03:45 +0000

    Rework thread management
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/protocols/ss7@12968 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ mtp/src/main/java/org/mobicents/protocols/ss7/mtp/MTPScheduler.java
@@ -0,0 +1,59 @@
+/*
+ * Mobicents, Communications Middleware
+ *
+ * Copyright (c) 2008, Red Hat Middleware LLC or third-party
+ * contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors. All third-party contributions are
+ * distributed under license by Red Hat Middleware LLC.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ *
+ * Boston, MA 02110-1301 USA
+ */
+
+package org.mobicents.protocols.ss7.mtp;
+
+/**
+ *
+ * @author kulikov
+ */
+public class MTPScheduler {
+    protected MTPTask[] tasks = new MTPTask[5];
+    
+    public void schedule(MTPTask task, int delay) {
+        task.deadLine= System.currentTimeMillis() + delay;
+        for (int i = 0; i < tasks.length; i++) {
+            if (tasks[i] == null) {
+                task.canceled = false;
+                tasks[i] = task;
+                task.index = i;
+                task.scheduler = this;
+                break;
+            }
+        }
+    }
+    
+    public void tick() {
+        long now = System.currentTimeMillis();
+        for (int i = 0; i < tasks.length; i++) {
+            if (tasks[i] != null && (tasks[i].deadLine <= now)) {
+                tasks[i].run();
+                tasks[i] = null;
+            } 
+        }
+    }
+}
---
+++ mtp/src/main/java/org/mobicents/protocols/ss7/mtp/MTPTask.java
@@ -0,0 +1,59 @@
+/*
+ * Mobicents, Communications Middleware
+ *
+ * Copyright (c) 2008, Red Hat Middleware LLC or third-party
+ * contributors as
+ * indicated by the @author tags or express copyright attribution
+ * statements applied by the authors. All third-party contributions are
+ * distributed under license by Red Hat Middleware LLC.
+ *
+ * This copyrighted material is made available to anyone wishing to use, modify,
+ * copy, or redistribute it subject to the terms and conditions of the GNU
+ * Lesser General Public License, as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this distribution; if not, write to:
+ * Free Software Foundation, Inc.
+ * 51 Franklin Street, Fifth Floor
+ *
+ * Boston, MA 02110-1301 USA
+ */
+package org.mobicents.protocols.ss7.mtp;
+
+/**
+ *
+ * @author kulikov
+ */
+public abstract class MTPTask implements Runnable {
+
+    protected boolean canceled = false;
+    protected long deadLine;
+    protected int index;
+    protected MTPScheduler scheduler;
+    
+    public void run() {
+        if (!canceled) {
+            perform();
+        }
+    }
+
+    public boolean isCanceled() {
+        return this.canceled;
+    }
+    
+    public abstract void perform();
+    
+    public void cancel() {
+        this.canceled = true;
+        //remove task from list
+        if (scheduler != null && (index >=0) && (index < scheduler.tasks.length)) {
+            scheduler.tasks[index] = null;
+        }
+    }
+}
