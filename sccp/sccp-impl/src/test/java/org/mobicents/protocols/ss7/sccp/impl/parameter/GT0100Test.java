a96226173f97a3a784205f1ccab4f5d1d790dfcb
oleg.kulikoff <oleg.kulikoff@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-08-24 08:22:59 +0000

    Issue 1749: Update GT0100 implementation
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/protocols/ss7@13766 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ sccp/sccp-impl/src/test/java/org/mobicents/protocols/ss7/sccp/impl/parameter/GT0100Test.java
@@ -0,0 +1,84 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.mobicents.protocols.ss7.sccp.impl.parameter;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Arrays;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.mobicents.protocols.ss7.indicator.GTIndicator;
+import org.mobicents.protocols.ss7.indicator.NatureOfAddress;
+import org.mobicents.protocols.ss7.indicator.NumberingPlan;
+
+/**
+ *
+ * @author kulikov
+ */
+public class GT0100Test {
+
+    private byte[] data = new byte[] {0, 0x12, 0x03, 0x09,0x32,0x26,0x59,0x18};
+    
+    public GT0100Test() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of decode method, of class GT0011.
+     */
+    @Test
+    public void testDecode() throws Exception {
+        //wrap data with input stream
+        ByteArrayInputStream in = new ByteArrayInputStream(data);
+        
+        //create GT object and read data from stream
+        GT0100 gt1 = new GT0100();
+        gt1.decode(in);
+        
+        //check results
+        assertEquals(0, gt1.getTranslationType());
+        assertEquals(NumberingPlan.ISDN_TELEPHONY, gt1.getNumberingPlan());
+        assertEquals("9023629581", gt1.getDigits());
+    }
+
+    /**
+     * Test of encode method, of class GT0011.
+     */
+    @Test
+    public void testEncode() throws Exception {
+        ByteArrayOutputStream bout = new ByteArrayOutputStream();
+        GT0100 gt = new GT0100(0, NumberingPlan.ISDN_TELEPHONY, NatureOfAddress.NATIONAL, "9023629581");
+        
+        gt.encode(bout);
+        
+        byte[] res = bout.toByteArray();
+        
+        boolean correct = Arrays.equals(data, res);        
+        assertTrue("Incorrect encoding", correct);
+    }
+
+}
