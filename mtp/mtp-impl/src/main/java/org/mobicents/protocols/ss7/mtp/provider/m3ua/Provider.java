6b720193bb2ecee2cf798a474b4fe19e16389919
baranowb <baranowb@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-09-16 17:21:35 +0000

    [SS7] add todo:
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/protocols/ss7@14124 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- mtp/mtp-impl/src/main/java/org/mobicents/protocols/ss7/mtp/provider/m3ua/Provider.java
+++ mtp/mtp-impl/src/main/java/org/mobicents/protocols/ss7/mtp/provider/m3ua/Provider.java
@@ -95,6 +95,7 @@ public class Provider implements MtpProvider, Runnable {
      * .Properties)
      */
     public void configure(Properties p) throws ConfigurationException {
+    	//TODO: implement this.
     }
 
     /*
