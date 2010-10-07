58a3ec5ed1683040d8fb98e5b49d11774ef21979
oleg.kulikoff <oleg.kulikoff@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-10-07 10:42:59 +0000

    Restore code
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/protocols/ss7@14362 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ sccp/sccp-impl/src/main/java/org/mobicents/protocols/ss7/sccp/impl/DefaultRouterImpl.java
@@ -0,0 +1,43 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright XXXX, Red Hat Middleware LLC, and individual contributors as indicated
+ * by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a full listing
+ * of individual contributors.
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU General Public License, v. 2.0.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License,
+ * v. 2.0 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+package org.mobicents.protocols.ss7.sccp.impl;
+
+import org.mobicents.protocols.ss7.sccp.Router;
+import org.mobicents.protocols.ss7.sccp.parameter.SccpAddress;
+
+/**
+ *
+ * @author kulikov
+ */
+public class DefaultRouterImpl implements Router {
+    private SccpStackImpl stack;
+    
+    public DefaultRouterImpl(SccpStackImpl stack) {
+        this.stack = stack;
+    }
+
+    public int getRoute(SccpAddress calledPartyAddress, SccpAddress callingPartyAddress) {
+        if (stack.linksets != null && stack.linksets.size() > 0) {
+            return stack.linksets.get(0).getAdjacentPointCode();
+        }
+        return -1;
+    }
+    
+    
+}
