37a0f6ec4aa9197747a42129d0176e98d117272f
oleg.kulikoff <oleg.kulikoff@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-12-22 06:46:26 +0000

    Issue #1922
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/protocols/ss7@16008 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ service/src/main/java/org/mobicents/ss7/SS7Service.java
@@ -0,0 +1,124 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright XXXX, Red Hat Middleware LLC, and individual contributors as indicated
+ * by the @authors tag. All rights reserved.
+ * See the copyright.txt in the distribution for a full listing
+ * of individual contributors.
+ * This copyrighted material is made available to anyone wishing to use,
+ * modify, copy, or redistribute it subject to the terms and conditions
+ * of the GNU General Public License, v. 2.0.
+ * This program is distributed in the hope that it will be useful, but WITHOUT A
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License,
+ * v. 2.0 along with this distribution; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+package org.mobicents.ss7;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import org.apache.log4j.Logger;
+import org.jboss.system.ServiceMBeanSupport;
+import org.mobicents.protocols.ss7.mtp.oam.LinksetManager;
+import org.mobicents.protocols.ss7.sccp.impl.SccpStackImpl;
+
+/**
+ *
+ * @author kulikov
+ */
+public class SS7Service extends ServiceMBeanSupport implements SS7ServiceMBean {
+
+    private ShellExecutor shell = new ShellExecutor();
+    
+    private LinksetManager linksetManager;    
+    private SccpStackImpl sccpStack;
+    
+    private String path;
+    private String jndiName;
+
+    private Logger logger = Logger.getLogger(SS7Service.class);
+    
+    @Override
+    public void startService() throws Exception {
+        //starting sccp router
+        logger.info("Starting SCCP stack...");
+        
+        sccpStack = new SccpStackImpl();
+        sccpStack.setConfigPath(path);
+        
+//        sccpStack.setLinksets(linksets);
+        sccpStack.start();
+        rebind(sccpStack);
+        
+        logger.info("SCCP stack Started. SccpProvider bound to " + this.jndiName);
+        
+        logger.info("Starting SS7 management shell environment");
+        shell.start();
+        
+        logger.info("Started SS7 service");
+    }
+
+    public void setJndiName(String jndiName) {
+        this.jndiName = jndiName;
+    }
+
+    public String getJndiName() {
+        return jndiName;
+    }
+    
+    public void setConfigPath(String path) {
+        this.path = path;
+    }
+    
+    @Override
+    public void stopService() {
+        try {
+            unbind(jndiName);
+        } catch (Exception e) {
+        }
+        logger.info("Stopped SS7 service");
+    }
+
+    public void setLinksetManager(LinksetManager manager) {
+        this.linksetManager = manager;
+    }
+
+    public LinksetManager getLinksetManager() {
+        return linksetManager;
+    }
+    
+    /**
+     * Binds trunk object to the JNDI under the jndiName.
+     */
+    private void rebind(SccpStackImpl stack) throws NamingException {
+        Context ctx = new InitialContext();
+        String tokens[] = jndiName.split("/");
+
+        for (int i = 0; i < tokens.length - 1; i++) {
+            if (tokens[i].trim().length() > 0) {
+                try {
+                    ctx = (Context) ctx.lookup(tokens[i]);
+                } catch (NamingException e) {
+                    ctx = ctx.createSubcontext(tokens[i]);
+                }
+            }
+        }
+
+        ctx.bind(tokens[tokens.length - 1], stack.getSccpProvider());
+    }
+    
+    /**
+     * Unbounds object under specified name.
+     * 
+     * @param jndiName
+     *            the JNDI name of the object to be unbound.
+     */
+    private void unbind(String jndiName) throws NamingException {
+        InitialContext initialContext = new InitialContext();
+        initialContext.unbind(jndiName);
+    }
+    
+}
