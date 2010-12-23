025c34e5059d9884bb8ded13323d8a1f85eabb50
amit.bhayani@gmail.com <amit.bhayani@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2010-12-23 10:44:49 +0000

    git-svn-id: https://mobicents.googlecode.com/svn/trunk/protocols/ss7@16024 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- management/shell/src/main/java/org/mobicents/ss7/management/transceiver/ShellSelectableChannel.java
+++ management/shell/src/main/java/org/mobicents/ss7/management/transceiver/ShellSelectableChannel.java
@@ -9,11 +9,6 @@ public abstract class ShellSelectableChannel {
     //underlying network channel
     protected SelectableChannel channel;
 
-    /**
-     * (Non Java-doc.)
-     * 
-     * @see  org.mobicents.protocols.ss7.m3ua.M3UASelectableChannel#register(org.mobicents.protocols.ss7.m3ua.M3UASelector, int);
-     */
     public ShellSelectionKey register(ShellSelector selector, int ops) throws ClosedChannelException {
         SelectionKey k = channel.register(selector.selector, ops);
         ShellSelectionKey key = new ShellSelectionKey(this, k);
