60e38b781b957e7dad7aa1a67876702a6347f01d
amit.bhayani@gmail.com <amit.bhayani@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2011-01-05 10:30:24 +0000

    git-svn-id: https://mobicents.googlecode.com/svn/trunk/protocols/ss7@16175 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- map/map-impl/src/main/java/MAPExample.java
+++ map/map-impl/src/main/java/MAPExample.java
@@ -1,8 +1,13 @@
+import javax.naming.NamingException;
+
+import org.mobicents.protocols.ss7.indicator.NatureOfAddress;
+import org.mobicents.protocols.ss7.map.MAPStackImpl;
 import org.mobicents.protocols.ss7.map.api.MAPApplicationContext;
 import org.mobicents.protocols.ss7.map.api.MAPDialog;
 import org.mobicents.protocols.ss7.map.api.MAPDialogListener;
 import org.mobicents.protocols.ss7.map.api.MAPProvider;
 import org.mobicents.protocols.ss7.map.api.MAPServiceListener;
+import org.mobicents.protocols.ss7.map.api.MAPStack;
 import org.mobicents.protocols.ss7.map.api.MapServiceFactory;
 import org.mobicents.protocols.ss7.map.api.dialog.AddressNature;
 import org.mobicents.protocols.ss7.map.api.dialog.AddressString;
@@ -16,97 +21,184 @@ import org.mobicents.protocols.ss7.map.api.dialog.NumberingPlan;
 import org.mobicents.protocols.ss7.map.api.service.supplementary.ProcessUnstructuredSSIndication;
 import org.mobicents.protocols.ss7.map.api.service.supplementary.USSDString;
 import org.mobicents.protocols.ss7.map.api.service.supplementary.UnstructuredSSIndication;
+import org.mobicents.protocols.ss7.sccp.SccpProvider;
+import org.mobicents.protocols.ss7.sccp.parameter.GlobalTitle;
 import org.mobicents.protocols.ss7.sccp.parameter.SccpAddress;
 
 public class MAPExample implements MAPDialogListener, MAPServiceListener {
 
-	MAPProvider mapProvider;
+    private MAPStack mapStack;
+    private MAPProvider mapProvider;
 
-	MapServiceFactory servFact = mapProvider.getMapServiceFactory();
+    MapServiceFactory servFact;
 
-	SccpAddress destAddress = null;
+    SccpAddress destAddress = null;
 
-	// The address created by passing the AddressNature, NumberingPlan and
-	// actual address
-	AddressString destReference = servFact.createAddressString(
-			AddressNature.international_number, NumberingPlan.land_mobile,
-			"204208300008002");
+    // The address created by passing the AddressNature, NumberingPlan and
+    // actual address
+    AddressString destReference = servFact.createAddressString(
+            AddressNature.international_number, NumberingPlan.land_mobile,
+            "204208300008002");
 
-	SccpAddress origAddress = null;
+    SccpAddress origAddress = null;
 
-	AddressString origReference = servFact.createAddressString(
-			AddressNature.international_number, NumberingPlan.ISDN,
-			"31628968300");
+    AddressString origReference = servFact.createAddressString(
+            AddressNature.international_number, NumberingPlan.ISDN,
+            "31628968300");
 
-	public void run() throws Exception {
+    MAPExample(SccpProvider sccpPprovider, SccpAddress address,
+            SccpAddress remoteAddress) {
+        origAddress = address;
+        destAddress = remoteAddress;
 
-		// First create Dialog
-		MAPDialog mapDialog = mapProvider.createNewDialog(
-				MAPApplicationContext.networkUnstructuredSsContextV2,
-				destAddress, destReference, origAddress, origReference);
+        mapStack = new MAPStackImpl(sccpPprovider, origAddress);
+        mapProvider = mapStack.getMAPProvider();
+        servFact = mapProvider.getMapServiceFactory();
 
-		// The dataCodingScheme is still byte, as I am not exactly getting how
-		// to encode/decode this.
-		byte ussdDataCodingScheme = 0x0f;
+        mapProvider.addMAPDialogListener(this);
+        mapProvider.addMAPServiceListener(this);
+    }
 
-		// USSD String: *125*+31628839999#
-		// The Charset is null, here we let system use default Charset (UTF-7 as
-		// explained in GSM 03.38. However if MAP User wants, it can set its own
-		// impl of Charset
-		USSDString ussdString = servFact.createUSSDString("*125*+31628839999#",
-				null);
-		
-		
-		AddressString msisdn = this.servFact
-		.createAddressString(AddressNature.international_number,
-				NumberingPlan.ISDN, "31628838002");				
+    private static SccpProvider getSccpProvider()
 
-		mapDialog.addProcessUnstructuredSSRequest(ussdDataCodingScheme,
-				ussdString, msisdn);
+    throws NamingException
 
-		// This will initiate the TC-BEGIN with INVOKE component
-		mapDialog.send();
-	}
+    {
 
-	public void onMAPAcceptInfo(MAPAcceptInfo mapAccptInfo) {
-		// TODO Auto-generated method stub
+//        // no arg is ok, if we run in JBoss
+//
+//        InitialContext ctx = new InitialContext();
+//
+//        try {
+//
+//            String adapterName = "jmx/invoker/RMIAdaptor";
+//
+//            Object obj = ctx.lookup(adapterName);
+//
+//            if (!(obj instanceof RMIAdaptor))
+//
+//            {
+//
+//                throw new ClassCastException
+//
+//                ("Object not of type: RMIAdaptorImpl, but: " +
+//
+//                (obj == null ? "not found" : obj.getClass().getName()));
+//
+//            }
+//
+//            // SS7Service is JMX bean, lets use it to get
+//
+//            MBeanServerConnection con = createMBeanServerConnection();
+//
+//            ObjectName on = new ObjectName(
+//                    "org.mobicents.ss7:service=SS7Service");
+//
+//            String providerJndiName = con.invoke(on, "getJndiName", null, null);
+//
+//            InitialContext ctx = new InitialContext();
+//
+//            return (SccpProvider) ctx.lookup(providerJndiName);
+//
+//        } finally
+//
+//        {
+//
+//            ctx.close();
+//
+//        }
+        return null;
 
-	}
+    }
 
-	public void onMAPCloseInfo(MAPCloseInfo mapCloseInfo) {
-		// TODO Auto-generated method stub
+    public void run() throws Exception {
 
-	}
+        // First create Dialog
+        MAPDialog mapDialog = mapProvider.createNewDialog(
+                MAPApplicationContext.networkUnstructuredSsContextV2,
+                destAddress, destReference, origAddress, origReference);
 
-	public void onMAPOpenInfo(MAPOpenInfo mapOpenInfo) {
-		// TODO Auto-generated method stub
+        // The dataCodingScheme is still byte, as I am not exactly getting how
+        // to encode/decode this.
+        byte ussdDataCodingScheme = 0x0f;
 
-	}
+        // USSD String: *125*+31628839999#
+        // The Charset is null, here we let system use default Charset (UTF-7 as
+        // explained in GSM 03.38. However if MAP User wants, it can set its own
+        // impl of Charset
+        USSDString ussdString = servFact.createUSSDString("*125*+31628839999#",
+                null);
 
-	public void onMAPProviderAbortInfo(MAPProviderAbortInfo mapProviderAbortInfo) {
-		// TODO Auto-generated method stub
+        AddressString msisdn = this.servFact.createAddressString(
+                AddressNature.international_number, NumberingPlan.ISDN,
+                "31628838002");
 
-	}
+        mapDialog.addProcessUnstructuredSSRequest(ussdDataCodingScheme,
+                ussdString, msisdn);
 
-	public void onMAPRefuseInfo(MAPRefuseInfo mapRefuseInfo) {
-		// TODO Auto-generated method stub
+        // This will initiate the TC-BEGIN with INVOKE component
+        mapDialog.send();
+    }
 
-	}
+    public void onMAPAcceptInfo(MAPAcceptInfo mapAccptInfo) {
+        // TODO Auto-generated method stub
 
-	public void onMAPUserAbortInfo(MAPUserAbortInfo mapUserAbortInfo) {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    public void onMAPCloseInfo(MAPCloseInfo mapCloseInfo) {
+        // TODO Auto-generated method stub
 
-	public void onProcessUnstructuredSSIndication(
-			ProcessUnstructuredSSIndication procUnstrInd) {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    public void onMAPOpenInfo(MAPOpenInfo mapOpenInfo) {
+        // TODO Auto-generated method stub
 
-	public void onUnstructuredSSIndication(UnstructuredSSIndication unstrInd) {
-		// TODO Auto-generated method stub
+    }
 
-	}
+    public void onMAPProviderAbortInfo(MAPProviderAbortInfo mapProviderAbortInfo) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void onMAPRefuseInfo(MAPRefuseInfo mapRefuseInfo) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void onMAPUserAbortInfo(MAPUserAbortInfo mapUserAbortInfo) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void onProcessUnstructuredSSIndication(
+            ProcessUnstructuredSSIndication procUnstrInd) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void onUnstructuredSSIndication(UnstructuredSSIndication unstrInd) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public static void main(String[] args) {
+
+        int translationType = 0;
+
+        int subSystemNumber = 0;
+
+//        GlobalTitle gt = GlobalTitle.getInstance(translationType,
+//                NumberingPlan.ISDN_MOBILE, NatureOfAddress.NATIONAL, "1234");
+//
+//        SccpAddress localAddress = new SccpAddress(gt, 0);
+//
+//        gt = GlobalTitle.getInstance(translationType,
+//                NumberingPlan.ISDN_MOBILE, NatureOfAddress.NATIONAL, "1572582");
+//
+//        SccpAddress remoteAddress = new SccpAddress(gt, 0);
+//
+//        MAPExample c = new MAPExample(getSccpProvider(), localAddress, remoteAddress);
+
+    }
 
 }
