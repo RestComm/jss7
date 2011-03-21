e03bc3f4b8e4cefa28aba314bf52447249df6d50
amit.bhayani@gmail.com <amit.bhayani@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2011-03-21 05:21:07 +0000

    Fix for  http://code.google.com/p/mobicents/issues/detail?id=2438:	Dialogic native code fails to compile
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/protocols/ss7@17412 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- hardware/dialogic/java/src/main/java/org/mobicents/ss7/hardware/dialogic/InterProcessCommunicator.java
+++ hardware/dialogic/java/src/main/java/org/mobicents/ss7/hardware/dialogic/InterProcessCommunicator.java
@@ -1,26 +1,36 @@
 /*
- * The Java Call Control API for CAMEL 2
+ * JBoss, Home of Professional Open Source
+ * Copyright ${year}, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
  *
- * The source code contained in this file is in in the public domain.
- * It can be used in any project or product without prior permission,
- * license or royalty payments. There is  NO WARRANTY OF ANY KIND,
- * EXPRESS, IMPLIED OR STATUTORY, INCLUDING, WITHOUT LIMITATION,
- * THE IMPLIED WARRANTY OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE,
- * AND DATA ACCURACY.  We do not warrant or make any representations
- * regarding the use of the software or the  results thereof, including
- * but not limited to the correctness, accuracy, reliability or
- * usefulness of the software.
- */
-
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */ 
 package org.mobicents.ss7.hardware.dialogic;
 
 import java.io.IOException;
 
 /**
- *
+ * @author amit bhayani
  * @author Oleg Kulikov
  */
 public class InterProcessCommunicator {
+	
+	private final static String LIB_NAME = "mobicents-dialogic-linux";
+	
     /** The identifier of the originated module */
     private int source;
     /** The identifier of the destination module */
@@ -89,6 +99,11 @@ public class InterProcessCommunicator {
     private native int send(int source, int destination, byte[] buffer);
     
     static {
-        System.loadLibrary("ipc");
+        try {
+            System.loadLibrary(LIB_NAME);
+            System.out.println("Loaded library mobicents-dialogic-linux");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }    
 }
