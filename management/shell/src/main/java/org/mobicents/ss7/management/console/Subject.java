0328a2adffa7adffe3c95e68eb1afceabf4ebc36
amit.bhayani@gmail.com <amit.bhayani@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2011-05-06 03:41:19 +0000

    Changed CLI command from SCCPRULE to SCCP
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/protocols/ss7@18356 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- management/shell/src/main/java/org/mobicents/ss7/management/console/Subject.java
+++ management/shell/src/main/java/org/mobicents/ss7/management/console/Subject.java
@@ -30,50 +30,50 @@ package org.mobicents.ss7.management.console;
  */
 public enum Subject {
 
-    /**
-     * linkset subject. Any command to manipulate the linkset, should begin with
-     * linkset subject
-     */
-    LINKSET("linkset"),
+	/**
+	 * linkset subject. Any command to manipulate the linkset, should begin with
+	 * linkset subject
+	 */
+	LINKSET("linkset"),
 
-    SCCPRULE("sccprule"),
-    /**
-     * M3UA Subject. Any command to manage the M3UA should begin with m3ua
-     * subject
-     */
-    M3UA("m3ua");
+	SCCP("sccp"),
+	/**
+	 * M3UA Subject. Any command to manage the M3UA should begin with m3ua
+	 * subject
+	 */
+	M3UA("m3ua");
 
-    private String subject = null;
+	private String subject = null;
 
-    private Subject(String subject) {
-        this.subject = subject;
-    }
+	private Subject(String subject) {
+		this.subject = subject;
+	}
 
-    /**
-     * get the string representation of this subject enum
-     * 
-     * @return
-     */
-    public String getSubject() {
-        return subject;
-    }
+	/**
+	 * get the string representation of this subject enum
+	 * 
+	 * @return
+	 */
+	public String getSubject() {
+		return subject;
+	}
 
-    /**
-     * get the corresponding enum for passed subject String. Returns null if
-     * there is no subject enum defined for passed subject string
-     * 
-     * @param subject
-     * @return
-     */
-    public static Subject getSubject(String subject) {
-        if (subject.compareTo(LINKSET.getSubject()) == 0) {
-            return LINKSET;
-        } else if (subject.compareTo(M3UA.getSubject()) == 0) {
-            return M3UA;
-        } else if (subject.compareTo(SCCPRULE.getSubject()) == 0){
-            return SCCPRULE;
-        }
-        return null;
-    }
+	/**
+	 * get the corresponding enum for passed subject String. Returns null if
+	 * there is no subject enum defined for passed subject string
+	 * 
+	 * @param subject
+	 * @return
+	 */
+	public static Subject getSubject(String subject) {
+		if (subject.equals(LINKSET.getSubject())) {
+			return LINKSET;
+		} else if (subject.equals(M3UA.getSubject())) {
+			return M3UA;
+		} else if (subject.equals(SCCP.getSubject())) {
+			return SCCP;
+		}
+		return null;
+	}
 
 }
