d6029abb863c2a1d0b4daae45055247a16050ec3
amit.bhayani@gmail.com <amit.bhayani@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2011-05-19 12:05:26 +0000

    updated license header
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/protocols/ss7@18522 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- hardware/dahdi/java/src/main/java/org/mobicents/ss7/hardware/dahdi/DahdiMtp3UserPart.java
+++ hardware/dahdi/java/src/main/java/org/mobicents/ss7/hardware/dahdi/DahdiMtp3UserPart.java
@@ -18,7 +18,8 @@
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- */ 
+ */
+
 package org.mobicents.ss7.hardware.dahdi;
 
 import java.io.IOException;
