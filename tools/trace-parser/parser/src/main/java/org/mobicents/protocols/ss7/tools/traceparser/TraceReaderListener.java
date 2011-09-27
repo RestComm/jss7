761dd1814677a73dad4a5ed187472385629e5e76
amit.bhayani@gmail.com <amit.bhayani@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2011-09-27 10:18:46 +0000

    creating 1.x branch of SS7
    git-svn-id: https://mobicents.googlecode.com/svn/branches/protocols/ss7/1.x@20626 bf0df8d0-2c1f-0410-b170-bd30377b63dc

---
+++ tools/trace-parser/parser/src/main/java/org/mobicents/protocols/ss7/tools/traceparser/TraceReaderListener.java
@@ -0,0 +1,40 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2011, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+
+package org.mobicents.protocols.ss7.tools.traceparser;
+
+/**
+ * 
+ * @author sergey vetyutnev
+ * 
+ */
+public interface TraceReaderListener {
+
+	/**
+	 * Deliver the ss7 message data.
+	 * Data is a raw data that includes BSN+Bib + FSN+Fib + LI + SIO + SIF
+	 * 
+	 * @param data
+	 */
+	public void ss7Message(byte[] data) throws TraceReaderException;
+
+}
