316f830ba013c9882e3076d09cf71d590660b920
amit.bhayani@gmail.com <amit.bhayani@gmail.com@bf0df8d0-2c1f-0410-b170-bd30377b63dc>
2011-12-14 13:19:17 +0000

    Fixes from branch 1.x
    git-svn-id: https://mobicents.googlecode.com/svn/trunk/protocols/ss7@21257 bf0df8d0-2c1f-0410-b170-bd30377b63dc

--- map/map-impl/src/test/java/org/mobicents/protocols/ss7/map/functional/SccpHarness.java
+++ map/map-impl/src/test/java/org/mobicents/protocols/ss7/map/functional/SccpHarness.java
@@ -40,10 +40,10 @@ import org.mobicents.protocols.ss7.sccp.impl.router.Router;
  */
 public abstract class SccpHarness {
 
-	protected SccpStackImpl sccpStack1 = new SccpStackImpl();
+	protected SccpStackImpl sccpStack1 = new SccpStackImpl("MapTestSccpStack1");
 	protected SccpProvider sccpProvider1 = sccpStack1.getSccpProvider();
 
-	protected SccpStackImpl sccpStack2 = new SccpStackImpl();
+	protected SccpStackImpl sccpStack2 = new SccpStackImpl("MapTestSccpStack2");
 	protected SccpProvider sccpProvider2 = sccpStack2.getSccpProvider();
 
 //	private ConcurrentLinkedQueue<byte[]> data1 = new ConcurrentLinkedQueue<byte[]>();
