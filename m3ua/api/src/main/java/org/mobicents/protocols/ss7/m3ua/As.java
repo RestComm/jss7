e0f9cabe4dc5869ce5e47e200d46cdc667dce61a
amit.bhayani@gmail.com <amit.bhayani@gmail.com>
2013-01-10 22:34:31 +0530

    More Java Docs

--- m3ua/api/src/main/java/org/mobicents/protocols/ss7/m3ua/As.java
+++ m3ua/api/src/main/java/org/mobicents/protocols/ss7/m3ua/As.java
@@ -28,6 +28,15 @@ import org.mobicents.protocols.ss7.m3ua.parameter.RoutingContext;
 import org.mobicents.protocols.ss7.m3ua.parameter.TrafficModeType;
 
 /**
+ * <p>
+ * Application Server (AS) is a logical entity serving a specific Routing Key.
+ * An example of an Application Server is a virtual switch element handling all
+ * call processing for a signalling relation, identified by an SS7 DPC/OPC.
+ * Another example is a virtual database element, handling all HLR transactions
+ * for a particular SS7 SIO/DPC/OPC combination. The AS contains a set of one or
+ * more unique Application Server Processes, of which one or more is normally
+ * actively processing traffic
+ * </p>
  * 
  * @author amit bhayani
  * 
@@ -35,30 +44,102 @@ import org.mobicents.protocols.ss7.m3ua.parameter.TrafficModeType;
 
 public interface As {
 
+	/**
+	 * Each As in M3UA stack is uniquely identified by its name
+	 * 
+	 * @return name of this As
+	 */
 	public String getName();
 
+	/**
+	 * Returns true if atleast one {@link Asp} in this As is ACTIVE and
+	 * exchanging payload
+	 * 
+	 * @return
+	 */
 	public boolean isConnected();
 
+	/**
+	 * Returns true if state of this As is ACTIVE
+	 * 
+	 * @return
+	 */
 	public boolean isUp();
 
+	/**
+	 * {@link RoutingContext} associated with this As. Configuring routing
+	 * context is optional and can return null if not configured. Note that
+	 * there is a 1:1 relationship between an AS and a Routing Key
+	 * 
+	 * @return Routing Key
+	 */
 	public RoutingContext getRoutingContext();
 
+	/**
+	 * The {@link Functionality} of this As
+	 * 
+	 * @return
+	 */
 	public Functionality getFunctionality();
 
+	/**
+	 * {@link ExchangeType} for this As
+	 * 
+	 * @return
+	 */
 	public ExchangeType getExchangeType();
 
+	/**
+	 * {@link IPSPType} for this As. This is useful only if
+	 * {@link Functionality} for this As is IPSP
+	 * 
+	 * @return
+	 */
 	public IPSPType getIpspType();
 
+	/**
+	 * {@link NetworkAppearance} for this As
+	 * 
+	 * @return
+	 */
 	public NetworkAppearance getNetworkAppearance();
 
+	/**
+	 * {@link TrafficModeType} for this As
+	 * 
+	 * @return
+	 */
 	public TrafficModeType getTrafficModeType();
 
+	/**
+	 * Default {@link TrafficModeType} that this As will assume if
+	 * TrafficModeType is not negotiated or not set when defining this As
+	 * 
+	 * @return
+	 */
 	public TrafficModeType getDefaultTrafficModeType();
 
+	/**
+	 * The minimu number of {@link Asp} that should be ACTIVE before this As
+	 * becomes ACTIVE. This value is useful only if {@link TrafficModeType} is
+	 * loadshare
+	 * 
+	 * @return
+	 */
 	public int getMinAspActiveForLb();
 
+	/**
+	 * Number of {@link Asp} assigned to this As
+	 * 
+	 * @return
+	 */
 	public List<Asp> getAspList();
-	
+
+	/**
+	 * Current state of this As
+	 * 
+	 * @return
+	 */
 	public State getState();
 
 }
--- m3ua/api/src/main/java/org/mobicents/protocols/ss7/m3ua/ExchangeType.java
+++ m3ua/api/src/main/java/org/mobicents/protocols/ss7/m3ua/ExchangeType.java
@@ -22,9 +22,27 @@
 package org.mobicents.protocols.ss7.m3ua;
 
 /**
+ * <p>
+ * Two modes of handshake is defined
+ * </p>
+ * <ol>
+ * <li>
+ * Single Exchange (SE) model : Only a single exchange of ASP Traffic
+ * Maintenance (ASPTM) Messages and ASP State Maintenance (ASPSM) Messages is
+ * needed to change the {@link As} states. This means that a set of requests
+ * from one end and acknowledgements from the other will be enough.</li>
+ * <li>
+ * Double Exchange (DE) model : A double exchange of ASPTM and ASPSM messages is
+ * normally needed. When using double exchanges for ASPSM messages, the
+ * management of the connection in the two directions is considered independent.
+ * This means that connections from As-A to As-B is handled independently of
+ * connections from As-B to As-A. Therefore, it could happen that only one of
+ * the two directions is activated or closed, while the other remains in the
+ * same state as it was.</li>
+ * </ol>
  * 
  * @author amit bhayani
- *
+ * 
  */
 public enum ExchangeType {
 	SE("SE"), DE("DE");
@@ -47,7 +65,7 @@ public enum ExchangeType {
 			return null;
 		}
 	}
-	
+
 	public String getType() {
 		return this.type;
 	}
