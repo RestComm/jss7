58993d4e676d5d9f41e180e1c5f514391cf33109
alerant.appngin <alerant.appngin@gmail.com>
2015-04-08 15:35:29 +0200

    +implements NamedTCListener
    Fix conflicts in:
    	map/map-impl/src/main/java/org/mobicents/protocols/ss7/map/MAPProviderImpl.java

--- map/map-impl/src/main/java/org/mobicents/protocols/ss7/map/MAPProviderImpl.java
+++ map/map-impl/src/main/java/org/mobicents/protocols/ss7/map/MAPProviderImpl.java
@@ -85,9 +85,9 @@ import org.mobicents.protocols.ss7.map.service.sms.MAPServiceSmsImpl;
 import org.mobicents.protocols.ss7.map.service.supplementary.MAPServiceSupplementaryImpl;
 import org.mobicents.protocols.ss7.tcap.DialogImpl;
 import org.mobicents.protocols.ss7.tcap.api.MessageType;
+import org.mobicents.protocols.ss7.tcap.api.NamedTCListener;
 import org.mobicents.protocols.ss7.tcap.api.TCAPProvider;
 import org.mobicents.protocols.ss7.tcap.api.TCAPSendException;
-import org.mobicents.protocols.ss7.tcap.api.TCListener;
 import org.mobicents.protocols.ss7.tcap.api.tc.component.InvokeClass;
 import org.mobicents.protocols.ss7.tcap.api.tc.dialog.Dialog;
 import org.mobicents.protocols.ss7.tcap.api.tc.dialog.events.TCBeginIndication;
@@ -133,7 +133,7 @@ import org.mobicents.protocols.ss7.tcap.asn.comp.ReturnResultProblemType;
  * @author sergey vetyutnev
  *
  */
-public class MAPProviderImpl implements MAPProvider, TCListener {
+public class MAPProviderImpl implements MAPProvider, NamedTCListener {
 
     protected final transient Logger loger;
 
@@ -162,6 +162,11 @@ public class MAPProviderImpl implements MAPProvider, TCListener {
     private final transient MAPServiceSupplementary mapServiceSupplementary = new MAPServiceSupplementaryImpl(this);
     private final transient MAPServiceSms mapServiceSms = new MAPServiceSmsImpl(this);
     private final transient MAPServiceLsm mapServiceLsm = new MAPServiceLsmImpl(this);
+    protected String name;
+
+    protected MAPProviderImpl() {
+        this.loger = Logger.getLogger(MAPStackImpl.class.getCanonicalName());
+    }
 
     /**
      * public common methods
@@ -170,6 +175,7 @@ public class MAPProviderImpl implements MAPProvider, TCListener {
     public MAPProviderImpl(String name, TCAPProvider tcapProvider) {
         this.loger = Logger.getLogger(MAPStackImpl.class.getCanonicalName() + "-" + name);
 
+        this.name = name;
         this.tcapProvider = tcapProvider;
 
         this.mapServices.add(this.mapServiceMobility);
@@ -179,6 +185,10 @@ public class MAPProviderImpl implements MAPProvider, TCListener {
         this.mapServices.add(this.mapServiceSupplementary);
         this.mapServices.add(this.mapServiceSms);
         this.mapServices.add(this.mapServiceLsm);
+    }
+
+    public String getName() {
+        return this.name;
     }
 
     public TCAPProvider getTCAPProvider() {
