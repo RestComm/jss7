a72073948bc94c9a6b738b6f5de2f3132cec3ed3
sergey vetyutnev <serg.vetyutnev@gmail.com>
2015-12-23 18:06:54 +0100

    Fixing bug in decoding of MWStatus MAP primitive

--- map/map-api/src/main/java/org/mobicents/protocols/ss7/map/api/service/sms/MWStatus.java
+++ map/map-api/src/main/java/org/mobicents/protocols/ss7/map/api/service/sms/MWStatus.java
@@ -26,9 +26,13 @@ import java.io.Serializable;
 
 /**
  *
- * MW-Status ::= BIT STRING { sc-AddressNotIncluded (0), mnrf-Set (1), mcef-Set (2) , mnrg-Set (3)} (SIZE (6..16)) -- exception
- * handling: -- bits 4 to 15 shall be ignored if received and not understood
- *
+<code>
+MW-Status ::= BIT STRING {
+  sc-AddressNotIncluded (0), mnrf-Set (1), mcef-Set (2) , mnrg-Set (3)
+} (SIZE (6..16))
+-- exception handling:
+-- bits 4 to 15 shall be ignored if received and not understood
+</code>
  *
  *
  * @author sergey vetyutnev
--- map/map-impl/src/main/java/org/mobicents/protocols/ss7/map/service/sms/MWStatusImpl.java
+++ map/map-impl/src/main/java/org/mobicents/protocols/ss7/map/service/sms/MWStatusImpl.java
@@ -22,37 +22,28 @@
 
 package org.mobicents.protocols.ss7.map.service.sms;
 
-import java.io.IOException;
-
-import org.mobicents.protocols.asn.AsnException;
-import org.mobicents.protocols.asn.AsnInputStream;
-import org.mobicents.protocols.asn.AsnOutputStream;
-import org.mobicents.protocols.asn.BitSetStrictLength;
-import org.mobicents.protocols.asn.Tag;
-import org.mobicents.protocols.ss7.map.api.MAPException;
-import org.mobicents.protocols.ss7.map.api.MAPParsingComponentException;
-import org.mobicents.protocols.ss7.map.api.MAPParsingComponentExceptionReason;
 import org.mobicents.protocols.ss7.map.api.service.sms.MWStatus;
-import org.mobicents.protocols.ss7.map.primitives.MAPAsnPrimitive;
+import org.mobicents.protocols.ss7.map.primitives.BitStringBase;
 
 /**
  *
  * @author sergey vetyutnev
  *
  */
-public class MWStatusImpl implements MWStatus, MAPAsnPrimitive {
+public class MWStatusImpl extends BitStringBase implements MWStatus {
 
     private static final int _INDEX_ScAddressNotIncluded = 0;
     private static final int _INDEX_MnrfSet = 1;
     private static final int _INDEX_McefSet = 2;
     private static final int _INDEX_MnrgSet = 3;
 
-    private BitSetStrictLength bitString = new BitSetStrictLength(6);
-
     public MWStatusImpl() {
+        super(4, 16, 6, "MWStatus");
     }
 
     public MWStatusImpl(boolean scAddressNotIncluded, boolean mnrfSet, boolean mcefSet, boolean mnrgSet) {
+        super(4, 16, 6, "MWStatus");
+
         if (scAddressNotIncluded)
             this.bitString.set(_INDEX_ScAddressNotIncluded);
         if (mnrfSet)
@@ -79,85 +70,21 @@ public class MWStatusImpl implements MWStatus, MAPAsnPrimitive {
         return this.bitString.get(_INDEX_MnrgSet);
     }
 
-    public int getTag() throws MAPException {
-        return Tag.STRING_BIT;
-    }
-
-    public int getTagClass() {
-        return Tag.CLASS_UNIVERSAL;
-    }
-
-    public boolean getIsPrimitive() {
-        return true;
-    }
-
-    public void decodeAll(AsnInputStream ansIS) throws MAPParsingComponentException {
-
-        try {
-            int length = ansIS.readLength();
-            this._decode(ansIS, length);
-        } catch (IOException e) {
-            throw new MAPParsingComponentException("IOException when decoding MWStatus: " + e.getMessage(), e,
-                    MAPParsingComponentExceptionReason.MistypedParameter);
-        } catch (AsnException e) {
-            throw new MAPParsingComponentException("AsnException when decoding MWStatus: " + e.getMessage(), e,
-                    MAPParsingComponentExceptionReason.MistypedParameter);
-        }
-    }
-
-    public void decodeData(AsnInputStream ansIS, int length) throws MAPParsingComponentException {
-
-        try {
-            this._decode(ansIS, length);
-        } catch (IOException e) {
-            throw new MAPParsingComponentException("IOException when decoding MWStatus: " + e.getMessage(), e,
-                    MAPParsingComponentExceptionReason.MistypedParameter);
-        } catch (AsnException e) {
-            throw new MAPParsingComponentException("AsnException when decoding MWStatus: " + e.getMessage(), e,
-                    MAPParsingComponentExceptionReason.MistypedParameter);
-        }
-    }
-
-    private void _decode(AsnInputStream ansIS, int length) throws MAPParsingComponentException, IOException, AsnException {
-        if (length == 0 || length > 2)
-            throw new MAPParsingComponentException(
-                    "Error decoding MWStatus: the MWStatus field must contain from 1 or 2 octets. Contains: " + length,
-                    MAPParsingComponentExceptionReason.MistypedParameter);
-
-        this.bitString = ansIS.readBitStringData(length);
-    }
-
-    public void encodeAll(AsnOutputStream asnOs) throws MAPException {
-
-        this.encodeAll(asnOs, Tag.CLASS_UNIVERSAL, Tag.STRING_BIT);
-    }
-
-    public void encodeAll(AsnOutputStream asnOs, int tagClass, int tag) throws MAPException {
-
-        try {
-            asnOs.writeTag(tagClass, true, tag);
-            int pos = asnOs.StartContentDefiniteLength();
-            this.encodeData(asnOs);
-            asnOs.FinalizeContent(pos);
-        } catch (AsnException e) {
-            throw new MAPException("AsnException when encoding MWStatus: " + e.getMessage(), e);
-        }
-    }
-
-    public void encodeData(AsnOutputStream asnOs) throws MAPException {
-        try {
-            asnOs.writeBitStringData(this.bitString);
-        } catch (IOException e) {
-            throw new MAPException("IOException when encoding MWStatus: " + e.getMessage(), e);
-        } catch (AsnException e) {
-            throw new MAPException("AsnException when encoding MWStatus: " + e.getMessage(), e);
-        }
-    }
-
     @Override
     public String toString() {
-        return "MWStatus [ScAddressNotIncluded=" + this.getScAddressNotIncluded() + ", MnrfSet=" + this.getMnrfSet()
-                + ", McefSet=" + this.getMcefSet() + ", MnrgSet=" + this.getMnrgSet() + "]";
+        StringBuilder sb = new StringBuilder();
+        sb.append(_PrimitiveName);
+        sb.append(" [");
+        if (this.getScAddressNotIncluded())
+            sb.append("ScAddressNotIncluded, ");
+        if (this.getMnrfSet())
+            sb.append("MnrfSet, ");
+        if (this.getMcefSet())
+            sb.append("McefSet, ");
+        if (this.getMnrgSet())
+            sb.append("MnrgSet, ");
+        sb.append("]");
+        return sb.toString();
     }
 
 }
