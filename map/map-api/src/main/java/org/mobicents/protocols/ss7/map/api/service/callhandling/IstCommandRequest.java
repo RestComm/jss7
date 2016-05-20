d37f85c5a8b5dd9cb13554783322b05f13ea9dfc
Grzegorz Figiel <grzegorz.figiel@pro-ids.com>
2016-05-21 01:29:10 +0200

    CheckStyle report indicated corrections

--- map/map-api/src/main/java/org/mobicents/protocols/ss7/map/api/service/callhandling/IstCommandRequest.java
+++ map/map-api/src/main/java/org/mobicents/protocols/ss7/map/api/service/callhandling/IstCommandRequest.java
@@ -29,21 +29,21 @@ import org.mobicents.protocols.ss7.map.api.primitives.MAPExtensionContainer;
  * MAP V3:
  *
  *
-<code>
-ist-Command OPERATION::= {
-  --Timer m 
-  ARGUMENT IST-CommandArg
-  RESULT IST-CommandRes -- optional
-  ERRORS { unexpectedDataValue | resourceLimitation | unknownSubscriber | systemFailure | facilityNotSupported}
-  CODE local:88
-}
-
-IST-CommandArg ::= SEQUENCE{
-  imsi                [0] IMSI,
-  extensionContainer  [1] ExtensionContainer OPTIONAL,
-  ...
-}
-</code>
+ * <code>
+ * ist-Command OPERATION::= {
+ *   --Timer m
+ *   ARGUMENT IST-CommandArg
+ *   RESULT IST-CommandRes -- optional
+ *   ERRORS { unexpectedDataValue | resourceLimitation | unknownSubscriber | systemFailure | facilityNotSupported}
+ *   CODE local:88
+ * }
+ *
+ *  IST-CommandArg ::= SEQUENCE{
+ *   imsi                [0] IMSI,
+ *   extensionContainer  [1] ExtensionContainer OPTIONAL,
+ *   ...
+ * }
+ * </code>
  *
  * @author sergey vetyutnev
  *
