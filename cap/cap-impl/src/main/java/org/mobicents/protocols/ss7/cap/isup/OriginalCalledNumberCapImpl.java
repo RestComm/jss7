b15af5b976e987bc6b16c0eb9775a0beb431421e
sergey vetyutnev <serg.vetyutnev@gmail.com>
2016-10-14 16:41:58 +0200

    Fixing #172: OriginalCalledNumberCap CAP message

--- cap/cap-impl/src/main/java/org/mobicents/protocols/ss7/cap/isup/OriginalCalledNumberCapImpl.java
+++ cap/cap-impl/src/main/java/org/mobicents/protocols/ss7/cap/isup/OriginalCalledNumberCapImpl.java
@@ -22,20 +22,12 @@
 
 package org.mobicents.protocols.ss7.cap.isup;
 
-import java.io.IOException;
-
 import javolution.xml.XMLFormat;
 import javolution.xml.stream.XMLStreamException;
 
-import org.mobicents.protocols.asn.AsnException;
-import org.mobicents.protocols.asn.AsnInputStream;
-import org.mobicents.protocols.asn.AsnOutputStream;
-import org.mobicents.protocols.asn.Tag;
 import org.mobicents.protocols.ss7.cap.api.CAPException;
-import org.mobicents.protocols.ss7.cap.api.CAPParsingComponentException;
-import org.mobicents.protocols.ss7.cap.api.CAPParsingComponentExceptionReason;
 import org.mobicents.protocols.ss7.cap.api.isup.OriginalCalledNumberCap;
-import org.mobicents.protocols.ss7.cap.primitives.CAPAsnPrimitive;
+import org.mobicents.protocols.ss7.cap.primitives.OctetStringBase;
 import org.mobicents.protocols.ss7.isup.ParameterException;
 import org.mobicents.protocols.ss7.isup.impl.message.parameter.OriginalCalledNumberImpl;
 import org.mobicents.protocols.ss7.isup.message.parameter.OriginalCalledNumber;
@@ -46,22 +38,21 @@ import org.mobicents.protocols.ss7.isup.message.parameter.OriginalCalledNumber;
  * @author sergey vetyutnev
  *
  */
-public class OriginalCalledNumberCapImpl implements OriginalCalledNumberCap, CAPAsnPrimitive {
-
-    public static final String _PrimitiveName = "OriginalCalledNumberCap";
+public class OriginalCalledNumberCapImpl extends OctetStringBase implements OriginalCalledNumberCap {
 
     private static final String ISUP_ORIGINAL_CALLED_NUMBER_XML = "isupOriginalCalledNumber";
 
-    private byte[] data;
-
     public OriginalCalledNumberCapImpl() {
+        super(2, 12, "OriginalCalledNumberCap");
     }
 
     public OriginalCalledNumberCapImpl(byte[] data) {
-        this.data = data;
+        super(2, 12, "OriginalCalledNumberCap", data);
     }
 
     public OriginalCalledNumberCapImpl(OriginalCalledNumber originalCalledNumber) throws CAPException {
+        super(2, 12, "OriginalCalledNumberCap");
+
         setOriginalCalledNumber(originalCalledNumber);
     }
 
@@ -95,95 +86,6 @@ public class OriginalCalledNumberCapImpl implements OriginalCalledNumberCap, CAP
     }
 
     @Override
-    public int getTag() throws CAPException {
-        return Tag.STRING_OCTET;
-    }
-
-    @Override
-    public int getTagClass() {
-        return Tag.CLASS_UNIVERSAL;
-    }
-
-    @Override
-    public boolean getIsPrimitive() {
-        return true;
-    }
-
-    @Override
-    public void decodeAll(AsnInputStream ansIS) throws CAPParsingComponentException {
-
-        try {
-            int length = ansIS.readLength();
-            this._decode(ansIS, length);
-        } catch (IOException e) {
-            throw new CAPParsingComponentException("IOException when decoding " + _PrimitiveName + ": " + e.getMessage(), e,
-                    CAPParsingComponentExceptionReason.MistypedParameter);
-        } catch (AsnException e) {
-            throw new CAPParsingComponentException("AsnException when decoding " + _PrimitiveName + ": " + e.getMessage(), e,
-                    CAPParsingComponentExceptionReason.MistypedParameter);
-        } catch (CAPParsingComponentException e) {
-            throw new CAPParsingComponentException("MAPParsingComponentException when decoding " + _PrimitiveName + ": "
-                    + e.getMessage(), e, CAPParsingComponentExceptionReason.MistypedParameter);
-        }
-    }
-
-    @Override
-    public void decodeData(AsnInputStream ansIS, int length) throws CAPParsingComponentException {
-
-        try {
-            this._decode(ansIS, length);
-        } catch (IOException e) {
-            throw new CAPParsingComponentException("IOException when decoding " + _PrimitiveName + ": " + e.getMessage(), e,
-                    CAPParsingComponentExceptionReason.MistypedParameter);
-        } catch (AsnException e) {
-            throw new CAPParsingComponentException("AsnException when decoding " + _PrimitiveName + ": " + e.getMessage(), e,
-                    CAPParsingComponentExceptionReason.MistypedParameter);
-        } catch (CAPParsingComponentException e) {
-            throw new CAPParsingComponentException("MAPParsingComponentException when decoding " + _PrimitiveName + ": "
-                    + e.getMessage(), e, CAPParsingComponentExceptionReason.MistypedParameter);
-        }
-    }
-
-    private void _decode(AsnInputStream ansIS, int length) throws CAPParsingComponentException,
-            IOException, AsnException {
-
-        this.data = ansIS.readOctetStringData(length);
-        if (this.data.length < 2 || this.data.length > 10)
-            throw new CAPParsingComponentException("Error while decoding " + _PrimitiveName
-                    + ": data must be from 2 to 10 bytes length, found: " + this.data.length,
-                    CAPParsingComponentExceptionReason.MistypedParameter);
-    }
-
-    @Override
-    public void encodeAll(AsnOutputStream asnOs) throws CAPException {
-        this.encodeAll(asnOs, this.getTagClass(), this.getTag());
-    }
-
-    @Override
-    public void encodeAll(AsnOutputStream asnOs, int tagClass, int tag) throws CAPException {
-
-        try {
-            asnOs.writeTag(tagClass, true, tag);
-            int pos = asnOs.StartContentDefiniteLength();
-            this.encodeData(asnOs);
-            asnOs.FinalizeContent(pos);
-        } catch (AsnException e) {
-            throw new CAPException("AsnException when encoding " + _PrimitiveName + ": " + e.getMessage(), e);
-        }
-    }
-
-    @Override
-    public void encodeData(AsnOutputStream asnOs) throws CAPException {
-
-        if (this.data == null)
-            throw new CAPException("data field must not be null");
-        if (this.data.length < 2 && this.data.length > 10)
-            throw new CAPException("data field length must be from 2 to 10");
-
-        asnOs.writeOctetStringData(data);
-    }
-
-    @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
         sb.append(_PrimitiveName);
@@ -191,7 +93,7 @@ public class OriginalCalledNumberCapImpl implements OriginalCalledNumberCap, CAP
 
         if (this.data != null) {
             sb.append("data=[");
-            sb.append(printDataArr(this.data));
+            sb.append(printDataArr());
             sb.append("]");
             try {
                 OriginalCalledNumber ocn = this.getOriginalCalledNumber();
@@ -202,16 +104,6 @@ public class OriginalCalledNumberCapImpl implements OriginalCalledNumberCap, CAP
         }
 
         sb.append("]");
-
-        return sb.toString();
-    }
-
-    private String printDataArr(byte[] arr) {
-        StringBuilder sb = new StringBuilder();
-        for (int b : arr) {
-            sb.append(b);
-            sb.append(", ");
-        }
 
         return sb.toString();
     }
