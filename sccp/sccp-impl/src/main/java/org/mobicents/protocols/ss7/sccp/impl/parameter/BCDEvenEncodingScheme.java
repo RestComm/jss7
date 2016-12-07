52dca3fa1d4296b2f9654ec7e9bf95beb0e619ca
Peramuthan <sriniramesh79@gmail.com>
2016-12-07 10:55:09 +0530

    Update BCDEvenEncodingScheme.java

--- sccp/sccp-impl/src/main/java/org/mobicents/protocols/ss7/sccp/impl/parameter/BCDEvenEncodingScheme.java
+++ sccp/sccp-impl/src/main/java/org/mobicents/protocols/ss7/sccp/impl/parameter/BCDEvenEncodingScheme.java
@@ -43,7 +43,7 @@ public class BCDEvenEncodingScheme extends DefaultEncodingScheme {
 
     @Override
     public EncodingSchemeType getType() {
-        return EncodingSchemeType.BCD_ODD;
+        return EncodingSchemeType.BCD_EVEN;
     }
 
     @Override
