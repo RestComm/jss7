f1e6ed993745fa4dfd2f8372bd3d9e7dc1c418b7
sergey vetyutnev <serg.vetyutnev@gmail.com>
2017-03-21 15:21:42 +0100

    Adding of testing (without config) test for Eri extentions inti SS7 Simulator

--- tools/simulator/core/src/main/java/org/mobicents/protocols/ss7/tools/simulator/tests/ussd/TestUssdClientMan.java
+++ tools/simulator/core/src/main/java/org/mobicents/protocols/ss7/tools/simulator/tests/ussd/TestUssdClientMan.java
@@ -91,6 +91,7 @@ import org.mobicents.protocols.ss7.map.api.service.supplementary.UnstructuredSSR
 import org.mobicents.protocols.ss7.map.api.service.supplementary.UnstructuredSSResponse;
 import org.mobicents.protocols.ss7.map.datacoding.CBSDataCodingSchemeImpl;
 import org.mobicents.protocols.ss7.map.dialog.MAPUserAbortChoiceImpl;
+import org.mobicents.protocols.ss7.map.primitives.AddressStringImpl;
 import org.mobicents.protocols.ss7.map.primitives.AlertingPatternImpl;
 import org.mobicents.protocols.ss7.tools.simulator.Stoppable;
 import org.mobicents.protocols.ss7.tools.simulator.common.AddressNatureType;
@@ -514,6 +515,16 @@ public class TestUssdClientMan extends TesterBase implements TestUssdClientManMB
             if (manualMode)
                 currentDialog = curDialog;
             invokeId = null;
+
+            boolean eriExtTest = false;
+//            eriExtTest = true;
+            if (eriExtTest) {
+                AddressString eriMsisdn = new AddressStringImpl(AddressNature.international_number, NumberingPlan.ISDN,
+                        "99000000000001");
+                AddressString eriVlrNo = new AddressStringImpl(AddressNature.international_number, NumberingPlan.ISDN,
+                        "99000000000002");
+                curDialog.addEricssonData(eriMsisdn, eriVlrNo);
+            }
 
             ISDNAddressString msisdn = null;
             if (this.testerHost.getConfigurationData().getTestUssdClientConfigurationData().getMsisdnAddress() != null
