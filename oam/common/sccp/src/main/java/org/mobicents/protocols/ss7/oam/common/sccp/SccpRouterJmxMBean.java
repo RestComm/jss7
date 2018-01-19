21e49266f4826631625faafbeec198ec7228faf2
knosach <kostyantyn.nosach@mobius-software.com>
2018-01-19 16:56:51 +0200

    closes #45 (GUI: adding of SCCP object modifying)

--- oam/common/sccp/src/main/java/org/mobicents/protocols/ss7/oam/common/sccp/SccpRouterJmxMBean.java
+++ oam/common/sccp/src/main/java/org/mobicents/protocols/ss7/oam/common/sccp/SccpRouterJmxMBean.java
@@ -35,4 +35,14 @@ public interface SccpRouterJmxMBean extends Router {
             int pAddressId, int sAddressId, int newCallingPartyAddressAddressId, int networkId, int callingai,
                  int callingpc, int callingssn, int callingtt, int callingnp,int callingnao, String callingdigits) throws Exception;
 
+    void addSccpLongMessageRule(int id, int firstSpc, int lastSpc, String ruleType) throws Exception;
+
+    void modifySccpLongMessageRule(int id, int firstSpc, int lastSpc, String ruleType) throws Exception;
+
+    void modifySccpRoutingAddress(int id, int ai, int pc, int ssn, int tt, int np, int nao, String digits) throws Exception;
+
+    void modifySccpRule(int id, String ruleType, String algo, String originationType, int ai, int pc, int ssn, int tt, int np, int nao, String digits, String mask,
+            int pAddressId, int sAddressId, int newCallingPartyAddressAddressId, int networkId, int callingai,
+            int callingpc, int callingssn, int callingtt, int callingnp,int callingnao, String callingdigits) throws Exception;
+
 }
