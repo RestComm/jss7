440e91fe82048d519118f6b368aac6c42e7d1212
sergey vetyutnev <serg.vetyutnev@gmail.com>
2018-02-13 13:04:10 +0100

    Fixing of M3uaManagementJmx - not all methods were implemented

--- oam/common/m3ua/src/main/java/org/mobicents/protocols/ss7/oam/common/m3ua/M3uaManagementJmx.java
+++ oam/common/m3ua/src/main/java/org/mobicents/protocols/ss7/oam/common/m3ua/M3uaManagementJmx.java
@@ -33,6 +33,7 @@ import org.mobicents.protocols.ss7.m3ua.AspFactory;
 import org.mobicents.protocols.ss7.m3ua.ExchangeType;
 import org.mobicents.protocols.ss7.m3ua.Functionality;
 import org.mobicents.protocols.ss7.m3ua.IPSPType;
+import org.mobicents.protocols.ss7.m3ua.M3UACounterProvider;
 import org.mobicents.protocols.ss7.m3ua.M3UAManagement;
 import org.mobicents.protocols.ss7.m3ua.M3UAManagementEventListener;
 import org.mobicents.protocols.ss7.m3ua.RouteAs;
@@ -840,5 +841,20 @@ public class M3uaManagementJmx implements M3uaManagementJmxMBean, M3UAManagement
     @Override
     public Map<String, RouteAs> getRoute() {
         return this.wrappedM3UAManagement.getRoute();
+    }
+
+    @Override
+    public M3UACounterProvider getCounterProvider() {
+        return this.wrappedM3UAManagement.getCounterProvider();
+    }
+
+    @Override
+    public void setStatisticsEnabled(boolean val) throws Exception {
+        this.wrappedM3UAManagement.setStatisticsEnabled(val);
+    }
+
+    @Override
+    public boolean getStatisticsEnabled() {
+        return this.wrappedM3UAManagement.getStatisticsEnabled();
     }
 }
